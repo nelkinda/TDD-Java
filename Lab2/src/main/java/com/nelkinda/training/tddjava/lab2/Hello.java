124ca8a2cc8990ba73b0f1feebf2d9dc3ed076fc
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2020-09-07 23:25:56 +0530

    Add Lab2.

---
+++ Lab2/src/main/java/com/nelkinda/training/tddjava/lab2/Hello.java
@@ -0,0 +1,12 @@
+package com.nelkinda.training.tddjava.lab2;
+
+public class Hello {
+    public static void main(final String... args) {
+        System.out.println(getMessage());
+    }
+
+    // TODO Remove private
+    private static String getMessage() {
+        return ""; // TODO Insert text
+    }
+}
---
+++ Lab2/src/test/java/com/nelkinda/training/tddjava/lab2/HelloTest.java
@@ -0,0 +1,13 @@
+package com.nelkinda.training.tddjava.lab2;
+
+// TODO: Add Import for @Test
+// TODO: Add Import for assertEquals
+
+class HelloTest {
+    // TODO: Add @Test annotation
+    void testHello() {
+        final String expected = "Hello, world!";
+        final String actual = Hello.getMessage();
+        /*TODO insert assertion method(expected, actual);*/
+    }
+}
