c0de98e4173412e53eaf397c33bd81b6f09f307b
Siddhesh Nikude <siddhesh.nikude@nelkinda.com>
2020-09-07 23:58:23 +0530

    Add Lab template.

---
+++ _Template/src/main/java/com/nelkinda/training/tddjava/lab1/Hello.java
@@ -0,0 +1,10 @@
+package com.nelkinda.training.tddjava.lab1;
+
+public enum Hello {
+    ;
+
+    @SuppressWarnings({"PMD.SystemPrintln", "java:S106"})
+    public static void main(final String... args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ _Template/src/test/java/com/nelkinda/training/tddjava/lab1/HelloTest.java
@@ -0,0 +1,37 @@
+package com.nelkinda.training.tddjava.lab1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.lang.String.format;
+import static java.nio.charset.Charset.defaultCharset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloTest {
+
+    private static final PrintStream ORIGINAL_STDOUT = System.out;
+    private ByteArrayOutputStream interceptedStdout;
+
+    @BeforeEach
+    void interceptStdout() {
+        interceptedStdout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(interceptedStdout));
+    }
+
+    @AfterEach
+    void restoreStdout() {
+        System.setOut(ORIGINAL_STDOUT);
+    }
+
+    @Test
+    void testHello() {
+        Hello.main();
+        final String expected = format("Hello, world!%n");
+        final String actual = interceptedStdout.toString(defaultCharset());
+        assertEquals(expected, actual);
+    }
+}
